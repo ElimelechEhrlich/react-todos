77c046aa93fc27c7c4e59944087135061741f803
ElimelechEhrlich <ee525566@gmail.com>
2026-02-26 19:03:21 +0200

    Pass onSaveUpdatedTodo to FormNewTodo component

--- src/pages/EditTodo.jsx
+++ src/pages/EditTodo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Form, useParams } from "react-router";
 import FormNewTodo from "../comps/FormTodo";
+import { onSaveUpdatedTodo } from "../services/onSaveForm.js
 
 export default function EditTodo() {
   const { id } = useParams();
@@ -12,7 +13,7 @@ export default function EditTodo() {
       className=" rounded items-center pt-5 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex  flex-col justify-center border border-black w-[40%]"
     >
       <h2 className="font-bold">עריכת משימה</h2>
-      <FormNewTodo id={id} />
+      <FormNewTodo onSave={onSaveUpdatedTodo} id={id} />
     </div>
   );
 }
