18cabc1b241e700af39a9eb4712d5f6a73b5d43a
ElimelechEhrlich <ee525566@gmail.com>
2026-02-26 20:00:32 +0200

    Update AddTodo.jsx

--- src/pages/AddTodo.jsx
+++ src/pages/AddTodo.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import FormNewTodo from "../comps/FormTodo";
 import TodosContext from "../context/TodosContext";
+import {onSaveNewTodo} from "../services/onSaveForm.js" 
 
 export default function AddTodo() {
   return (
@@ -9,7 +10,7 @@ export default function AddTodo() {
       className=" rounded items-center pt-5 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex  flex-col justify-center border border-black w-[40%]"
     >
       <h2 className="font-bold">יצירת משימה חדשה</h2>
-      <FormTodo onSave={onSave} />
+      <FormTodo onSave={onSaveNewTodo} />
     </div>
   );
 }
